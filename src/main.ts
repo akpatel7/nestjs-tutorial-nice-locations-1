521f210d5bb134049d4cf41684c4c5350b25398b
Amit Patel <amit.patel@sda.com>
2022-03-09 17:37:57 -0500

    imported config service in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  const config = app.get(ConfigService);
+  const viewsDirectory = config.get<string>('templates.path');
+  const publicDirectory = config.get<string>('public.path');
+  app.setBaseViewsDir(viewsDirectory);
+  app.useStaticAssets(publicDirectory);
 
   await app.listen(3000);
 }
